a7b71ef49be2b1bb83da004dc5ae87c7e9d08729
MahammadJafarli <mahammad.c@code.edu.az>
2020-11-05 19:01:39 +0400

    parse added

---
+++ FourSquareClone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FourSquareClone
+//
+//  Created by Mahammad Jafarli on 11/5/20.
+//  Copyright © 2020 Mahammad Jafarli. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
